1121e7a966b4314f5e3f171d2811e8973a3534b6
daishuai <867390427@qq.com>
2019-03-24 20:31:29 +0800

    add WebConfig

---
+++ src/main/java/com/daishuai/jenkins/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.daishuai.jenkins.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description: java类作用描述
+ * @Author: daishuai
+ * @CreateDate: 2019/3/24 20:30
+ * @Version: 1.0
+ * Copyright: Copyright (c) 2019
+ */
+@Configuration
+public class WebConfig {
+}
